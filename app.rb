64ea949472e82c26a7ca2a960c1eb23b795d870a
codeworm96 <codeworm96@outlook.com>
2016-01-24 15:59:33 +0800

    Basic routes

--- app.rb
+++ app.rb
@@ -1,3 +1,14 @@
 require_relative 'lib/beaker'
 
+class Beaker::Base
+  get '/' do
+    ['200', {'Content-Type' => 'text/html'}, ['Hello, world!']]
+  end
+
+  get /\w+/ do
+    ['200', {'Content-Type' => 'text/html'}, ['Regexp.']]
+  end
+
+end
+
 Beaker::Base.run!
--- lib/beaker/base.rb
+++ lib/beaker/base.rb
@@ -4,14 +4,23 @@ module Beaker
   class Base
     def call(env)
       @request = Rack::Request.new(env)
-      if @request.path_info == '/'
-        ['200', {'Content-Type' => 'text/html'}, ['Hello world!']]
-      else
-        ['404', {'Content-Type' => 'text/html'}, ['Not found!']]
+
+      # Can not handle error correctly now!
+      
+      self.class.routes[@request.request_method].each do |path, block|
+        return block.call if path.match @request.path_info
       end
+
+      ['404', {'Content-Type' => 'text/html'}, ['Not found!']]
     end
 
     class << self
+      attr_reader :routes
+
+      def reset!
+        @routes = {}
+      end
+
       def call(env)
         new.call(env)
       end
@@ -20,6 +29,56 @@ module Beaker
         handler = Rack::Handler.get('thin')
         handler.run(self, {Port: 4000, Host: '127.0.0.1'})
       end
+
+      def route(verb, path, &block)
+        # init list
+        @routes[verb] ||= []
+
+        @routes[verb] << [path, block]
+      end
+
+      def get(path, &block)
+        route 'GET', path, &block
+        # Defining 'GET' also automatically defines 'HEAD'
+        route 'HEAD', path, &block
+      end
+
+      def put(path, &block)
+        route 'PUT', path, &block
+      end
+
+      def post(path, &block)
+        route 'POST', path, &block
+      end
+
+      def delete(path, &block)
+        route 'DELETE', path, &block
+      end
+
+      def head(path, &block)
+        route 'HEAD', path, &block
+      end
+
+      def options(path, &block)
+        route 'OPTIONS', path, &block
+      end
+
+      def patch(path, &block)
+        route 'PATCH', path, &block
+      end
+
+      def link(path, &block)
+        route 'LINK', path, &block
+      end
+
+      def unlink(path, &block)
+        route 'UNLINK', path, &block
+      end
+
     end
+
+    # init routes
+    reset!
+
   end
 end
